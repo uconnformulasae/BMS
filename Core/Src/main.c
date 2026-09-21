03e0e01786adc5ca00ab8ace0ea867fac5561a21
harvanshpelia <harvanshpelia@gmail.com>
2026-09-21 11:45:55 -0400

    Enabled Hardware NSS
    Also added some basic test code

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -102,6 +102,9 @@ int main(void)
   MX_SPI1_Init();
   /* USER CODE BEGIN 2 */
 
+  uint8_t RDSID[2] = {0x00, 0x2C};
+  uint8_t SID[6] = {0x00};
+
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -109,7 +112,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	HAL_SPI_Transmit(&hspi1, RDSID, sizeof(RDSID)/sizeof(uint8_t), 1000);
+	HAL_SPI_Receive(&hspi1, SID, sizeof(SID)/sizeof(uint8_t), 1000);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -302,10 +306,10 @@ static void MX_SPI1_Init(void)
   hspi1.Instance = SPI1;
   hspi1.Init.Mode = SPI_MODE_MASTER;
   hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.DataSize = SPI_DATASIZE_16BIT;
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
   hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.NSS = SPI_NSS_HARD_OUTPUT;
   hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
@@ -340,17 +344,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(SPI1_NSS_GPIO_Port, SPI1_NSS_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOC, MCU_DISCHARGE_EN_Pin|MCU_CHARGE_EN_Pin, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin : SPI1_NSS_Pin */
-  GPIO_InitStruct.Pin = SPI1_NSS_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(SPI1_NSS_GPIO_Port, &GPIO_InitStruct);
 
   /*Configure GPIO pins : MCU_DISCHARGE_EN_Pin MCU_CHARGE_EN_Pin */
   GPIO_InitStruct.Pin = MCU_DISCHARGE_EN_Pin|MCU_CHARGE_EN_Pin;
